0cc973809776f641e5028fc9ea623010117db984
tesnimed <tesnimedlbi1@gmail.com>
2026-05-18 02:40:06 +0300

    Fix mixed content force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production' || env('APP_ENV') === 'local') {
+        // إجبار تشغيل الـ HTTPS عند رفع المشروع على السيرفر
+        if (config('app.env') === 'production' || env('APP_ENV') === 'production') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
