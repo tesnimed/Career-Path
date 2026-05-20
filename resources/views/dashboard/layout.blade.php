5f4d870df3a6a97a636fbf6570a8e6ebf302fb92
tesnimed <tesnimedlbi1@gmail.com>
2026-05-20 22:57:29 +0300

    dashboard css

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Kontrol Paneli')</title>
 
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
